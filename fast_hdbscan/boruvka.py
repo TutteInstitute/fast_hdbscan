49543434a1e21cfa48b41d0b78b9c9ffd9a90743
Leland McInnes <leland.mcinnes@gmail.com>
2023-05-08 11:19:59 -0400

    Update boruvka.py

--- fast_hdbscan/boruvka.py
+++ fast_hdbscan/boruvka.py
@@ -4,7 +4,7 @@ import numpy as np
 from .disjoint_set import ds_rank_create, ds_find, ds_union_by_rank
 from .numba_kdtree import parallel_tree_query, rdist, point_to_node_lower_bound_rdist
 
-@numba.njit(locals={"i": numba.int64})
+@numba.njit(locals={"i": numba.types.int64})
 def merge_components(disjoint_set, candidate_neighbors, candidate_neighbor_distances, point_components):
     component_edges = {np.int64(0): (np.int64(0), np.int64(1), np.float32(0.0)) for i in range(0)}
 
