3e00738b1fe0e880e69c852f75adf9d4d433991e
Leland McInnes <leland.mcinnes@gmail.com>
2024-06-16 17:17:48 -0400

    Update test_hdbscan.py

--- fast_hdbscan/tests/test_hdbscan.py
+++ fast_hdbscan/tests/test_hdbscan.py
@@ -149,7 +149,7 @@ def test_fhdbscan_allow_single_cluster_with_epsilon():
     ).fit(no_structure)
     unique_labels, counts = np.unique(c.labels_, return_counts=True)
     assert len(unique_labels) == 8
-    assert counts[unique_labels == -1] == 65
+    assert counts[unique_labels == -1] == 68
 
     # An epsilon of 0.2 will produce 2 noise points and 2 labels
     # Allow single cluster does not prevent applying the epsilon threshold.
