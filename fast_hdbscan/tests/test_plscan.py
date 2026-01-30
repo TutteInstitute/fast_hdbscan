3731612688c909e526cb2d18ee02e6be25684904
Leland McInnes <leland.mcinnes@gmail.com>
2026-01-30 16:41:46 +0000

    Update tests

--- fast_hdbscan/tests/test_plscan.py
+++ fast_hdbscan/tests/test_plscan.py
@@ -43,10 +43,10 @@ def test_plscan_badargs():
         PLSCAN(min_samples=-1).fit(X)
 
 
-
 def test_plscan_max_layers():
     model = PLSCAN(max_layers=1).fit(X)
     assert len(model.cluster_layers_) == 1
+
 
 def test_plscan_layer_similarity_threshold():
     # Lower threshold should allow more layers
@@ -54,10 +54,12 @@ def test_plscan_layer_similarity_threshold():
     model_high = PLSCAN(layer_similarity_threshold=0.9, max_layers=5).fit(X)
     assert len(model_low.cluster_layers_) >= len(model_high.cluster_layers_)
 
+
 def test_plscan_reproducibility():
     model1 = PLSCAN(reproducible=True).fit(X)
     model2 = PLSCAN(reproducible=True).fit(X)
     assert_array_equal(model1.labels_, model2.labels_)
+
 
 def test_plscan_base_n_clusters():
     # Should get close to requested n_clusters in first layer
@@ -67,12 +69,14 @@ def test_plscan_base_n_clusters():
     n_found = len(set(first_layer)) - int(-1 in first_layer)
     assert abs(n_found - requested) <= 1
 
+
 def test_plscan_multiple_layers():
     model = PLSCAN(max_layers=3, layer_similarity_threshold=0.1).fit(X)
     assert len(model.cluster_layers_) > 1
     # Each layer should have different clusterings
     unique_labels = [set(layer) for layer in model.cluster_layers_]
     assert len(set(map(tuple, unique_labels))) == len(model.cluster_layers_)
+
 
 def test_plscan_tree_generation():
     model = PLSCAN(max_layers=3).fit(X)
